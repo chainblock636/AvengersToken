3a5a6c2ac4f6b27bd792e37a9230204505b31cf9
chainblock636 <chainblock636@gmail.com>
2021-12-23 08:41:47 +0100

    staking page

--- client/src/GameProfile.js
+++ client/src/GameProfile.js
@@ -8,11 +8,34 @@ import Subscribe from './components/GameProfile/Subscribe'
 const GameProfile = () => {
     return (
         <div>
-            <Breadcrumb />
-            <GameProfileTop />
+            {/* <Breadcrumb /> */}
+            <section class="breadcrumb-area bonus">
+                
+            </section>
+            <section class="bonuse-section">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="header-area">
+                                <div class="row justify-content-center">
+                                    <div class="col-lg-8 text-center">
+                                        <h1>
+                                            Coming Soon
+                                        </h1>
+                                        <a href="#" class="mybtn2">
+                                            Buy Presale
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* <GameProfileTop />
             <UserMenu />
             <UserMainDash />
-            <Subscribe />
+            <Subscribe /> */}
         </div>
     )
 }
